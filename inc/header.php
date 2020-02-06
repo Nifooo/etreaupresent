5e4957da3af2acc029e71d2b5091b97148865162
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-02-06 10:28:57 +0100

    Commit 1 :
    -Header mis en place avec JS CSS
    -Img d'accueil mise en place en width : 100%;

---
+++ inc/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>Être au présent - massage bien être et hypnose : Alentours de périgueux</title>
+
+</head>
+<body>
+
+<header id="navbar">
+
+    <p>Naviguer en toute tranquilité</p>
+    <div id="myNav" class="overlay">
+
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+        <div class="overlay-content">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+
+    </div>
+
+    <div class="menuBurger" onclick="openNav()">
+        <div class="burger"></div>
+        <div class="burger"></div>
+        <div class="burger"></div>
+    </div>
+
+    <div class="clear"></div>
+
+</header>
