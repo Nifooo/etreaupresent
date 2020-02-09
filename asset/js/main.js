1b50b8bfbbccef1be101c92cbd696e22b0db9a7b
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-02-09 15:10:33 +0100

    taf

--- asset/js/main.js
+++ asset/js/main.js
@@ -41,3 +41,24 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
+
+
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 600, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
