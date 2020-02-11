76f1b4b2f3962f6fc6634a8e56174431b688d57b
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-02-11 16:15:33 +0100

    ouiii

--- massage.php
+++ massage.php
@@ -15,18 +15,20 @@ foreach ($massages as $massage) {
 
     ;; ?>
     <div id="listeMassage">
-
             <a href="details.php"><img src="asset/img/logo.png" alt="logo du site"></a>
 
-            <h3><?= $massage['nom']; ?></h3>
-            <p><?= $massage['prix'] . '€'; ?></p>
-            <p><?= $massage['time']; ?></p>
-            <span><?= $massage['description']; ?></span>
-
+            <div class="mass">
+            <a href="details.php"><h3><?= $massage['nom']; ?></h3></a>
+            <h4><?= $massage['prix'] . '€'; ?></h4>
+            <h4><?= $massage['time']; ?></h4>
+            <a href="details.php"><p>Voir plus -></p></a>
+            </div>
     </div>
 
 
 <?php } ?>
+
+<div class="clear"></div>
 
 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 <script src="asset/js/main.js"></script>
