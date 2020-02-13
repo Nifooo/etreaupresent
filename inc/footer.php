a420d9d81d5d8531fce041f36e9776a9d9acd541
Etienne Barassin <etiennebarassin@gmail.com>
2020-02-13 10:23:14 +0100

    taf

--- inc/footer.php
+++ inc/footer.php
@@ -2,27 +2,20 @@
     <div class="social">
         <h3>Me retrouver</h3><div class="barfooter"></div>
         <ul>
-            <li><img src="asset/img/instagram.svg">Insta</li>
-            <li><img src="asset/img/facebook.svg">fb</li>
-            <li><img src="asset/img/twitter.svg">twitter</li>
+            <li><img src="asset/img/instagram.svg"><p>Insta</p></li>
+            <li><img src="asset/img/facebook.svg"><p>fb</p></li>
+            <li><img src="asset/img/twitter.svg"><p>twitter</p></li>
         </ul>
     </div>
     <div class="found">
         <h3>Me contacter</h3><div class="barfooter"></div>
         <ul>
-            <li><img src="asset/img/phone.svg">Téléphone : </li>
-            <li><img src="asset/img/location.svg">Adresse : </li>
-            <li><img src="asset/img/mail.svg">Email : </li>
+            <li><img src="asset/img/phone.svg"><p>Téléphone : </p></li>
+            <li><img src="asset/img/location.svg"><p>Adresse : </p></li>
+            <li><img src="asset/img/mail.svg"><p>Email : </p></li>
         </ul>
     </div>
-    <div class="information">
-        <h3>Informations</h3><div class="barfooter"></div>
-        <ul>
-            <li><a href="cgu.php">> CGU</a></li>
-            <li><a href="index.php#who">> A propos de moi</a></li>
-            <li><a href="index.php#formulaire">> Contact</a></li>
-        </ul>
-    </div>
+
     <div class="photo">
         <h3>Mes photos</h3><div class="barfooter"></div>
         <ul>
@@ -32,6 +25,14 @@
             <li><img src=""></li>
             <li><img src=""></li>
             <li><img src=""></li>
+        </ul>
+    </div>
+    <div class="information">
+        <h3>Informations</h3><div class="barfooter"></div>
+        <ul>
+            <li><a href="cgu.php">> CGU</a></li>
+            <li><a href="index.php#who">> A propos de moi</a></li>
+            <li><a href="index.php#formulaire">> Contact</a></li>
         </ul>
     </div>
 
