ce4ca1d01a50aff8b668b960c131dce2accea63b
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-02-13 10:09:00 +0100

    tout

--- inc/footer.php
+++ inc/footer.php
@@ -39,6 +39,9 @@
 
 </footer>
 
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script src="asset/js/main.js"></script>
+
 </body>
 </html>
 
